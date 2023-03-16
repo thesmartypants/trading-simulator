8fa8179ff91fe46c85e39d863ca2682f938c1fe1
thesmartypants <alex@shmulevich.com>
2023-03-16 12:03:42 -0500

    added a moving avg predictor

--- main.py
+++ main.py
@@ -1,41 +1,41 @@
 import json
 import urllib.request
 import time
-from queue import queue
-from bank import bank
+from queue import Queue
+from bank import Bank
+from predictorma import PredictorMa
 
 cur = input('enter crypto currency: ')
 of_cur = input('official currency in your country ex: cad, usd, mxn: ')
 url = f'https://api.coingecko.com/api/v3/simple/price?ids={cur}&vs_currencies={of_cur}'
-prices = queue(3)
-cur_bal=float(input(f'how much would you be willing to invest? {of_cur} '))
+cur_bal = float(input(f'how much would you be willing to invest? {of_cur} '))
+# initialize components
+your_bank = Bank(cur_bal)
 
-your_bank=bank(cur_bal)
+predictor = PredictorMa()
 
 while 1:
-  req = urllib.request.urlopen(url).read().decode()
-  d = json.loads(req)
-  last_price = d[cur][of_cur]
+    # get latest price
+    req = urllib.request.urlopen(url).read().decode()
+    d = json.loads(req)
+    last_price = d[cur][of_cur]
+    print('\n')
+    print('moving avg prediction: ')
+    print('last price:', last_price)
 
-  prices.add(last_price)
-  moving_avg_last3 = prices.get_avg()
+    t = time.localtime()
+    current_time = time.strftime("%H:%M:%S", t)
+    print('time: ', current_time)
 
-  
-  print('last price:', last_price)
-  print('moving avg(3):', moving_avg_last3)
-  t = time.localtime()
-  current_time = time.strftime("%H:%M:%S", t)
-  print('time: ', current_time)
-  
-  if not prices.is_ready():
-    print('We dont know yet, the real moving avarage only shows up after the third cycle.')
-  elif last_price > moving_avg_last3:
-    print('sell')
-    your_bank.sell(last_price)
-  elif last_price < moving_avg_last3:
-    print('buy')
-    your_bank.buy(last_price)
-  your_bank.print_bals()
-  print('\n\n')
+    signal = predictor.get_signal(last_price)
+    print('moving avg(3):', predictor.cur_mov_avg)
+    print(signal)
+    if signal == 'sell':
+        your_bank.sell(last_price)
+    elif signal == 'buy':
+        your_bank.buy(last_price)
 
-  time.sleep(10)
+    your_bank.print_bals()
+    print('\n\n')
+
+    time.sleep(15)
--- queue.py
+++ queue.py
@@ -1,22 +1,22 @@
-class queue:
+class Queue:
 
-  def __init__(self, size):
-    self.size = size
-    self.arr = []
+    def __init__(self, size):
+        self.size = size
+        self.arr = []
 
-  def add(self, element):
-    self.arr.append(element)
-    if len(self.arr) > self.size:
-      self.arr.remove(self.arr[0])
+    def add(self, element):
+        self.arr.append(element)
+        if len(self.arr) > self.size:
+            self.arr.remove(self.arr[0])
 
-  def get_avg(self):
-    if self.is_ready():
-      return sum(self.arr) / self.size
-    else:
-      return 0 #todo: throw exception
+    def get_avg(self):
+        if self.is_ready():
+            return sum(self.arr) / self.size
+        else:
+            return 0  # todo: throw exception
 
-  def print_content(self):
-    print(self.arr)
-  
-  def is_ready(self):
-    return len(self.arr)==self.size
+    def print_content(self):
+        print(self.arr)
+
+    def is_ready(self):
+        return len(self.arr) == self.size
