20376c68ea2535496e663a2173b34ac4446f1600
thesmartypants <alex@shmulevich.com>
2023-03-16 12:03:42 -0500

    added a moving avg predictor

--- queue_test.py
+++ queue_test.py
@@ -1,25 +1,26 @@
-from queue import queue
-queue1=queue(3)
+from queue import Queue
+
+queue1 = Queue(3)
 queue1.add(1)
 queue1.print_content()
 queue1.add(2)
 if not queue1.is_ready():
-  print('ready ok')
+    print('ready ok')
 else:
-  print('ready failed')
+    print('ready failed')
 queue1.print_content()
 queue1.add(3)
 queue1.print_content()
 queue1.add(4)
 queue1.print_content()
 avg = queue1.get_avg()
-if avg == (2+3+4)/3:
-  print('avg ok')
+if avg == (2 + 3 + 4) / 3:
+    print('avg ok')
 else:
-  print('avg failed')
-  
+    print('avg failed')
+
 print(queue1.get_avg())
 if queue1.is_ready():
-  print('ready, ok.')
+    print('ready, ok.')
 else:
-  print('ready failed')
+    print('ready failed')
