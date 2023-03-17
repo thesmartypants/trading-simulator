bc0eb17cfd2a2f61c9c22a418b8d9e3eb0f03b83
Decrypter777 <107758654+thesmartypants@users.noreply.github.com>
2023-03-17 10:37:17 -0400

    Update PredictorAvg.py

--- PredictorAvg.py
+++ PredictorAvg.py
@@ -1,1 +1,12 @@
-
+class predict:
+  def __init__(self,hisory):
+    self.history=history
+  def give_signal(self,price):
+    self.history.append(price)
+    his_avg=sum(self.history)/len(self.history)
+    if his_avg>0:
+      return 'buy'
+    elif his_avg<0:
+      return 'sell'
+    else:
+      return ''
