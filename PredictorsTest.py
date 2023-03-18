81f85c2a4c9ec462e3b8c3a62f50af4cc766fa3c
Decrypter777 <107758654+thesmartypants@users.noreply.github.com>
2023-03-18 18:51:27 -0400

    Update PredictorsTest.py

--- PredictorsTest.py
+++ PredictorsTest.py
@@ -1,17 +1,15 @@
-class AvgPredict:
-  def __init__(self,history):
-    self.history=history
-  def give_signal(self,price):
-    self.history.append(price)
-    change_avg=0
-    for i in range (len(self.history)):
-      if i!=0:
-        change_avg+=self.history[i]-self.history[i-1]
-        
-    change_avg=change_avg/len(self.history)
-    if change_avg>0:
-      return 'buy'
-    elif change_avg<0:
-      return 'sell'
-    else:
-      return ''
+from PredictorAvg import AvgPredict
+from PredictorMa import PredictorMa
+
+Predictor1 = AvgPredict([1, 3, 6, 2, 4])
+if Predictor1.give_signal(7) == 'buy':
+  print('Avg predictor success')
+else:
+  print('Avg predictor failed')
+Predictor2=PredictorMa()
+Predictor2.get_signal(2)
+Predictor2.get_signal(4)
+if Predictor2.get_signal(7)=='sell':
+  print('Moving avg predictor success')
+else:
+  print('Moving avg predictor failed')
