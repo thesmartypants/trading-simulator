451cc1928d5e962bde6f95a19169bc7046fde51f
Decrypter777 <107758654+thesmartypants@users.noreply.github.com>
2023-03-22 18:16:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,46 +4,55 @@ import time
 from queue import Queue
 from bank import Bank
 from PredictorMa import PredictorMa
+from PredictorAvg import AvgPredict
 
+history=[]
 type=input('Moving avg or Change Avg (M/C): ')
 cur = input('enter crypto currency: ')
 of_cur = input('official currency in your country ex: cad, usd, mxn: ')
 url = f'https://api.coingecko.com/api/v3/simple/price?ids={cur}&vs_currencies={of_cur}'
 cur_bal = float(input(f'how much would you be willing to invest? {of_cur} '))
 if type=='C':
-  history=input('history: ')#change this line later
+  h=input('history(separate with coma): ')#change this line later
+  if h!='':
+    h.split(',')
+  for item in h:
+    history.append(int(item))
 # initialize components
 your_bank = Bank(cur_bal)
 
 predictorM = PredictorMa()
-predictorC=PredictorAvg(history)
+predictorC=AvgPredict(history)
 
-# get latest price
-req = urllib.request.urlopen(url).read().decode()
-d = json.loads(req)
-last_price = d[cur][of_cur]
+
 
 while 1:
+      # get latest price
+      req = urllib.request.urlopen(url).read().decode()
+      d = json.loads(req)
+      last_price = d[cur][of_cur]
       if type=='M':
         
         print('\n')
-        print('moving avg: '+str(predictor.cur_mov_avg))
         print('last price:', last_price)
     
         t = time.localtime()
         current_time = time.strftime("%H:%M:%S", t)
         print('time: ', current_time)
     
-        signal = predictor.get_signal(last_price)
-        print('moving avg(3):', predictor.cur_mov_avg)
+        signal = predictorM.get_signal(last_price)
+        print('moving avg(3):', predictorM.cur_mov_avg)
         print(signal)
       
       elif type=='C':
         print('\n')
+        print('last price:', last_price)
+        print('change avg:',predictorC.change_avg)
         t = time.localtime()
         current_time = time.strftime("%H:%M:%S", t)
         print('time: ', current_time)
-        signal=predictor.get_signal(last_price)
+        signal=predictorC.give_signal(last_price)
+        print('signal:',signal)
         
       if signal == 'sell':
         your_bank.sell(last_price)
@@ -54,4 +63,5 @@ while 1:
       your_bank.print_bals()
       print('\n\n')
 
-    time.sleep(15)
+      time.sleep(15)
+  
